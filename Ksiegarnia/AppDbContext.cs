44b9dcffcc429e82262db48949e8a00f4dded6c6
Lordziax <92054583+Lordziax@users.noreply.github.com>
2026-04-16 17:25:07 +0200

    xcxc

--- Ksiegarnia/AppDbContext.cs
+++ Ksiegarnia/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Ksiegarnia.Models;
+﻿using Ksiegarnia;
+using Ksiegarnia.Models;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ksiegarnia
@@ -11,11 +13,35 @@ namespace Ksiegarnia
 		public DbSet<Wypozyczenie> Wypozyczenia { get; set; }
 		public DbSet<Magazyn> Magazyn { get; set; }
 
-		protected override void OnConfiguring(DbContextOptionsBuilder options)
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			options.UseSqlServer(
-				@"Server=.\SQLEXPRESS;Database=Ksiegarnia;Trusted_Connection=True;TrustServerCertificate=True;"
-			);
+			if (!optionsBuilder.IsConfigured)
+			{
+				var baseConn = Environment.GetEnvironmentVariable("DB_CONN") ??
+							   Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection");
+
+				if (string.IsNullOrWhiteSpace(baseConn))
+				{
+					baseConn = "Server=localhost\\SQLEXPRESS;Database=Ksiegarnia;Trusted_Connection=True;TrustServerCertificate=True;";
+				}
+
+				var builder = new SqlConnectionStringBuilder(baseConn)
+				{
+					Encrypt = true,
+					PersistSecurityInfo = false,
+					ConnectTimeout = 30
+				};
+
+				// jeśli używamy LocalDB lub Integrated Security, wyłącz szyfrowanie (LocalDB nie wspiera)
+				if (!string.IsNullOrWhiteSpace(builder.DataSource) &&
+					(builder.DataSource.StartsWith("(localdb)", StringComparison.OrdinalIgnoreCase) || builder.IntegratedSecurity))
+				{
+					builder.Encrypt = false;
+				}
+
+				optionsBuilder.UseSqlServer(builder.ConnectionString);
+			}
 		}
+
 	}
 }
